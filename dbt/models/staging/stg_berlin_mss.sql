191c732fb7cae932cb90c84f7b5a6f9ecdb3f98b
Hoang Vu Tuyen <hoang.vutuyen@live.de>
2026-06-17 00:09:41 +0200

    Normalisierung entfernen, Werte sind bereits Prozent

--- dbt/models/staging/stg_berlin_mss.sql
+++ dbt/models/staging/stg_berlin_mss.sql
@@ -7,35 +7,15 @@ with source as (
 
 cleaned as (
     select
-        lor_key                                     as lor_key,
-        lor_name                                    as lor_name,
-        bezirk                                      as district,
+        lor_key,
+        lor_name,
+        bezirk                  as district,
         year,
 
-        -- Raten als Dezimal normalisieren (falls > 1 = Prozentwert)
-        case
-            when unemployment_rate > 1
-            then unemployment_rate / 100.0
-            else unemployment_rate
-        end                                         as unemployment_rate,
-
-        case
-            when child_poverty_rate > 1
-            then child_poverty_rate / 100.0
-            else child_poverty_rate
-        end                                         as child_poverty_rate,
-
-        case
-            when transfer_rate > 1
-            then transfer_rate / 100.0
-            else transfer_rate
-        end                                         as transfer_rate,
-
-        case
-            when youth_unemployment > 1
-            then youth_unemployment / 100.0
-            else youth_unemployment
-        end                                         as youth_unemployment_rate,
+        unemployment_rate,      -- bereits in %, kein / 100 nötig
+        child_poverty_rate,
+        transfer_rate,
+        youth_unemployment      as youth_unemployment_rate,
 
         geometry,
         ingested_at,
@@ -43,6 +23,8 @@ cleaned as (
 
     from source
     where lor_key is not null
+      and unemployment_rate is not null
+      and unemployment_rate >= 0   -- filtert Sentinel-Werte raus
 )
 
 select * from cleaned
